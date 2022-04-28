cea8863ecc3a05d578c9341cdb12bea04fa146b0
Graig KOLODZIEJCZYK <graig.kolodziejczyk@ynov.com>
2022-04-29 01:13:52 +0200

    all

--- App/Models/Race.cs
+++ App/Models/Race.cs
@@ -5,7 +5,7 @@ namespace App.Models
     public class Race
     {
         public int Id { get; set; }
-        public int PiloteId {get; set;}
+        public int PiloteId { get; set; }
         public string? Name { get; set; }
         public DateTime EventDate { get; set; }
 
